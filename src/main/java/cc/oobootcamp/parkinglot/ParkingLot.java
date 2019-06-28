ac49eb3cd6e97e8bf2da85f01ee569aac6cc3de2
xsu <xsu@thoughtworks.com>
2019-06-28 11:20:27 +0800

    can save the car in the parking lot.

--- src/main/java/cc/oobootcamp/parkinglot/ParkingLot.java
+++ src/main/java/cc/oobootcamp/parkinglot/ParkingLot.java
@@ -1,15 +1,25 @@
 package cc.oobootcamp.parkinglot;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class ParkingLot {
 
     private final int capacity;
+    private final List<Car> cars;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        this.cars = new ArrayList<>();
     }
 
-
     public Ticket park(Car car) {
+        this.cars.add(car);
         return new Ticket();
+    }
+
+    public Collection<Object> getAllCars() {
+        return new ArrayList<>(this.cars);
     }
 }
--- src/test/java/cc/oobootcamp/parkinglot/ParkingLotTest.java
+++ src/test/java/cc/oobootcamp/parkinglot/ParkingLotTest.java
@@ -2,17 +2,27 @@ package cc.oobootcamp.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
 
     @Test
-    void should_return_a_ticket_given_the_parking_lot_with_available_space() {
+    void should_return_a_ticket_when_park_car_given_the_parking_lot_with_available_space() {
         ParkingLot parkingLot = new ParkingLot(1);
         Car car = new Car();
 
         Ticket ticket = parkingLot.park(car);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_save_the_car_into_parking_lot_when_park_car_given_the_parking_lot_with_available_space() {
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car car = new Car();
+
+        parkingLot.park(car);
+
+        assertTrue(parkingLot.getAllCars().contains(car));
     }
 }
